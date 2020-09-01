62f01bfceac147533e684464c653cb4a0d547124
Jagrajan Bhullar <jag@jagrajan.com>
2020-08-31 23:38:51 -0700

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: 'us-east-1',
+    BUCKET: 'jag-notes-app-uploads',
+  },
+  apiGateway: {
+    REGION: 'us-east-1',
+    URL: 'https://ytbhavyfn9.execute-api.us-east-1.amazonaws.com/dev',
+  },
+  cognito: {
+    REGION: 'us-east-1',
+    USER_POOL_ID: 'us-east-1_JKS99YReX',
+    APP_CLIENT_ID: '7mjp6qstnjjihti5hk4ostpm86',
+    IDENTITY_POOL_ID: 'us-east-1:b00fa017-cc24-4369-9a92-481a455a26f5',
+  }
+}
